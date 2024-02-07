7ebc7ba06bb1a6c1168f70a117ceabf703217183
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-02-07 10:31:29 +0600

    added member dashboard Routes

--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -6,10 +6,10 @@ import { FaBlogger } from "react-icons/fa";
 import { FaUsersCog } from "react-icons/fa";
 import { LiaResearchgate } from "react-icons/lia";
 import { MdOutlinePendingActions } from "react-icons/md";
-
+import { CgProfile } from "react-icons/cg";
 import { GiArchiveResearch } from "react-icons/gi";
-import UserRole from "../../hooks/userRole";
 import useAuth from "../../hooks/useAuth";
+import { AiOutlineProfile } from "react-icons/ai";
 const AdminLayout = () => {
 
     const { user, logout } = useAuth();
@@ -35,8 +35,6 @@ const AdminLayout = () => {
         roleCheck()
     }
 
-    console.log(role);
-
     return (
         <div className="min-h-screen flex bg-white-900">
             <div className="sidebar min-h-screen w-[140px] md:w-[64px] overflow-hidden border-r md:hover:w-80 lg:w-64 lg:hover:w-64 hover:bg-white hover:shadow-lg">
@@ -45,87 +43,144 @@ const AdminLayout = () => {
                         <div className="w-max p-2.5">
                             <img src="https://i.ibb.co/HpYr1kc/logo.png" className="w-40" alt="" />
                         </div>
-                        <ul className="mt-6 space-y-2 tracking-wide">
-                            <li className="min-w-max">
-                                <a href="#" aria-label="dashboard" className="relative flex items-center space-x-4 bg-gradient-to-r from-sky-600 to-cyan-400 px-4 py-3 text-white">
-                                    <svg className="-ml-1 h-6 w-6" viewBox="0 0 24 24" fill="none">
-                                        <path d="M6 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8ZM6 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-1Z" className="fill-current text-cyan-400 dark:fill-slate-600"></path>
-                                        <path d="M13 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2V8Z" className="fill-current text-cyan-200 group-hover:text-cyan-300"></path>
-                                        <path d="M13 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-1Z" className="fill-current group-hover:text-sky-300"></path>
-                                    </svg>
-                                    <span className="-mr-1 font-medium">Dashboard</span>
-                                </a>
-                            </li>
-                            <li className="min-w-max">
-                                <Link to="/dashboard/addblog" className="bg group flex items-center space-x-4 rounded-full px-4 py-3 text-gray-600">
 
 
-                                    <FaBlogger className="group-hover:text-cyan-300" />
+                        {/* Admin Link */}
+                        {
+                            role === 'admin' && (
+                                <ul className="mt-6 space-y-2 tracking-wide">
+                                    <li className="min-w-max">
+                                        <a href="#" aria-label="dashboard" className="relative flex items-center space-x-4 bg-gradient-to-r from-sky-600 to-cyan-400 px-4 py-3 text-white">
+                                            <svg className="-ml-1 h-6 w-6" viewBox="0 0 24 24" fill="none">
+                                                <path d="M6 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8ZM6 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-1Z" className="fill-current text-cyan-400 dark:fill-slate-600"></path>
+                                                <path d="M13 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2V8Z" className="fill-current text-cyan-200 group-hover:text-cyan-300"></path>
+                                                <path d="M13 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-1Z" className="fill-current group-hover:text-sky-300"></path>
+                                            </svg>
+                                            <span className="-mr-1 font-medium">Dashboard</span>
+                                        </a>
+                                    </li>
+                                    <li className="min-w-max">
+                                        <Link to="/dashboard/addblog" className="bg group flex items-center space-x-4 rounded-full px-4 py-3 text-gray-600">
 
-                                    <span className="group-hover:text-gray-700">Add Blog</span>
-                                </Link>
-                            </li>
-                            <li className="min-w-max">
-                                <Link to="/dashboard/addresearch" className="bg group flex items-center space-x-4 rounded-full px-4 py-3 text-gray-600">
+
+                                            <FaBlogger className="group-hover:text-cyan-300" />
+
+                                            <span className="group-hover:text-gray-700">Add Blog</span>
+                                        </Link>
+                                    </li>
+                                    <li className="min-w-max">
+                                        <Link to="/dashboard/addresearch" className="bg group flex items-center space-x-4 rounded-full px-4 py-3 text-gray-600">
 
 
-                                    <GiArchiveResearch />
+                                            <GiArchiveResearch />
 
-                                    <span className="group-hover:text-gray-700">Add Research</span>
-                                </Link>
-                            </li>
-                            <li className="min-w-max">
-                                <Link to="/dashboard/addnews" className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                        <path className="fill-current text-gray-600 group-hover:text-cyan-600" fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd" />
-                                        <path className="fill-current text-gray-300 group-hover:text-cyan-300" d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z" />
-                                    </svg>
-                                    <span className="group-hover:text-gray-700">Add News</span>
-                                </Link>
-                            </li>
-                            <li className="min-w-max">
-                                <Link to="/dashboard/addadviser" className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
+                                            <span className="group-hover:text-gray-700">Add Research</span>
+                                        </Link>
+                                    </li>
+                                    <li className="min-w-max">
+                                        <Link to="/dashboard/addnews" className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                                <path className="fill-current text-gray-600 group-hover:text-cyan-600" fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd" />
+                                                <path className="fill-current text-gray-300 group-hover:text-cyan-300" d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z" />
+                                            </svg>
+                                            <span className="group-hover:text-gray-700">Add News</span>
+                                        </Link>
+                                    </li>
+                                    <li className="min-w-max">
+                                        <Link to="/dashboard/addadviser" className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
 
-                                    <MdNaturePeople className="group-hover:text-cyan-300" />
+                                            <MdNaturePeople className="group-hover:text-cyan-300" />
 
-                                    <span className="group-hover:text-gray-700">Add Adviser</span>
-                                </Link>
-                            </li>
-                            <li className="min-w-max">
-                                <Link to="/dashboard/addevent" className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
-                                    <RiCalendarEventFill className="group-hover:text-cyan-300" />
-                                    <span className="group-hover:text-gray-700">Add Event</span>
-                                </Link>
-                            </li>
-                            <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
-                            <li className="min-w-max">
-                                <Link to="/dashboard/manageblogs" className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
+                                            <span className="group-hover:text-gray-700">Add Adviser</span>
+                                        </Link>
+                                    </li>
+                                    <li className="min-w-max">
+                                        <Link to="/dashboard/addevent" className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
+                                            <RiCalendarEventFill className="group-hover:text-cyan-300" />
+                                            <span className="group-hover:text-gray-700">Add Event</span>
+                                        </Link>
+                                    </li>
+                                    <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+                                    <li className="min-w-max">
+                                        <Link to="/dashboard/manageblogs" className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
 
-                                    <FaBlogger className="group-hover:text-cyan-300" />
-                                    <span className="group-hover:text-gray-700">Manage Blogs</span>
-                                </Link>
-                            </li>
-                            <li className="min-w-max">
-                                <Link to="/dashboard/manageusers" className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
-                                    <FaUsersCog
-                                        className="group-hover:text-cyan-300" />
-                                    <span className="group-hover:text-gray-700">Manage Users</span>
-                                </Link>
-                            </li>
-                            <li className="min-w-max">
-                                <Link to="/dashboard/manageresearch" className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
-                                    <LiaResearchgate className="group-hover:text-cyan-300" />
-                                    <span className="group-hover:text-gray-700">Manage Research</span>
-                                </Link>
-                            </li>
-                            <li className="min-w-max">
-                                <Link to="/dashboard/managemresearch" className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
-                                    <MdOutlinePendingActions className="group-hover:text-cyan-300" />
-                                    <span className="group-hover:text-gray-700">Pending MResearch</span>
-                                </Link>
-                            </li>
-                        </ul>
+                                            <FaBlogger className="group-hover:text-cyan-300" />
+                                            <span className="group-hover:text-gray-700">Manage Blogs</span>
+                                        </Link>
+                                    </li>
+                                    <li className="min-w-max">
+                                        <Link to="/dashboard/manageusers" className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
+                                            <FaUsersCog
+                                                className="group-hover:text-cyan-300" />
+                                            <span className="group-hover:text-gray-700">Manage Users</span>
+                                        </Link>
+                                    </li>
+                                    <li className="min-w-max">
+                                        <Link to="/dashboard/manageresearch" className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
+                                            <LiaResearchgate className="group-hover:text-cyan-300" />
+                                            <span className="group-hover:text-gray-700">Manage Research</span>
+                                        </Link>
+                                    </li>
+                                    <li className="min-w-max">
+                                        <Link to="/dashboard/managemresearch" className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
+                                            <MdOutlinePendingActions className="group-hover:text-cyan-300" />
+                                            <span className="group-hover:text-gray-700">Pending MResearch</span>
+                                        </Link>
+                                    </li>
+                                </ul>
+                            )
+                        }
+
+                        {/* Faqulty Link */}
+
+                        {role === 'faculty' && (
+                            <ul className="mt-6 space-y-2 tracking-wide">
+                                <li className="min-w-max">
+                                    <a href="#" aria-label="dashboard" className="relative flex items-center space-x-4 bg-gradient-to-r from-sky-600 to-cyan-400 px-4 py-3 text-white">
+                                        <svg className="-ml-1 h-6 w-6" viewBox="0 0 24 24" fill="none">
+                                            <path d="M6 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8ZM6 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-1Z" className="fill-current text-cyan-400 dark:fill-slate-600"></path>
+                                            <path d="M13 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2V8Z" className="fill-current text-cyan-200 group-hover:text-cyan-300"></path>
+                                            <path d="M13 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-1Z" className="fill-current group-hover:text-sky-300"></path>
+                                        </svg>
+                                        <span className="-mr-1 font-medium">Dashboard</span>
+                                    </a>
+                                </li>
+
+
+
+                                <li className="min-w-max">
+                                    <Link to="/dashboard/manageresearch" className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
+                                        <CgProfile className="group-hover:text-cyan-300" />
+                                        <span className="group-hover:text-gray-700">Update Profile</span>
+                                    </Link>
+                                </li>
+                                <li className="min-w-max">
+                                    <Link to="/dashboard/addblog" className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
+
+                                        <FaBlogger className="group-hover:text-cyan-300" />
+                                        <span className="group-hover:text-gray-700">Add Blog</span>
+                                    </Link>
+                                </li>
+                                <li className="min-w-max">
+                                    <Link to="/dashboard/manageresearch" className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
+                                        <LiaResearchgate className="group-hover:text-cyan-300" />
+                                        <span className="group-hover:text-gray-700">Add Research</span>
+                                    </Link>
+                                </li>
+                                <li className="min-w-max">
+                                    <Link to="/dashboard/manageresearch" className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
+                                        <AiOutlineProfile className="group-hover:text-cyan-300" />
+                                        <span className="group-hover:text-gray-700">Manage Research</span>
+                                    </Link>
+                                </li>
+                                <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+                            </ul>
+                        )}
                     </div>
+
+
+
+
                     <div className="w-max -mb-3">
                         <a href="#" className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 group-hover:fill-cyan-600" viewBox="0 0 20 20" fill="currentColor">
@@ -139,7 +194,7 @@ const AdminLayout = () => {
             <div>
                 <Outlet></Outlet>
             </div>
-        </div>
+        </div >
     );
 };
 
