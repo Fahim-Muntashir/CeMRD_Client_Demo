4654d9abd41896e212030591614bc88e990ca652
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-03-06 21:51:13 +0600

    added commit

--- src/pages/Admin/Add/AddResearch.jsx
+++ src/pages/Admin/Add/AddResearch.jsx
@@ -11,7 +11,7 @@ const AddResearch = () => {
   const handleResearchSubmit = (event) => {
     event.preventDefault();
 
-    fetch("https://cemrd-online.vercel.app/api/research/addresearch/", {
+    fetch("http://localhost:5000/api/research/addresearch/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
--- src/pages/Research/Research.jsx
+++ src/pages/Research/Research.jsx
@@ -35,7 +35,7 @@ const Research = () => {
             <div className="container mx-auto space-y-12">
               <div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row">
                 <img
-                  src="https://source.unsplash.com/640x480/?1"
+                  src={research.img}
                   alt=""
                   className="h-80 dark:bg-gray-500 aspect-video"
                 />
@@ -43,59 +43,16 @@ const Research = () => {
                   <span className="text-xs uppercase dark:text-gray-400">
                     Join, it s free
                   </span>
-                  <h3 className="text-3xl font-bold">
-                    We re not reinventing the wheel
-                  </h3>
+                  <h3 className="text-3xl font-bold">{research.title}</h3>
                   <p className="my-6 dark:text-gray-400">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Dolor aliquam possimus quas, error esse quos.
                   </p>
-                  <button type="button" className="self-start">
-                    Action
-                  </button>
-                </div>
-              </div>
-              <div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row-reverse">
-                <img
-                  src="https://source.unsplash.com/640x480/?2"
-                  alt=""
-                  className="h-80 dark:bg-gray-500 aspect-video"
-                />
-                <div className="flex flex-col justify-center flex-1 p-6 dark:bg-gray-900">
-                  <span className="text-xs uppercase dark:text-gray-400">
-                    Join, it s free
-                  </span>
-                  <h3 className="text-3xl font-bold">
-                    We re not reinventing the wheel
-                  </h3>
-                  <p className="my-6 dark:text-gray-400">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Dolor aliquam possimus quas, error esse quos.
-                  </p>
-                  <button type="button" className="self-start">
-                    Action
-                  </button>
-                </div>
-              </div>
-              <div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row">
-                <img
-                  src="https://source.unsplash.com/640x480/?3"
-                  alt=""
-                  className="h-80 dark:bg-gray-500 aspect-video"
-                />
-                <div className="flex flex-col justify-center flex-1 p-6 dark:bg-gray-900">
-                  <span className="text-xs uppercase dark:text-gray-400">
-                    Join, it s free
-                  </span>
-                  <h3 className="text-3xl font-bold">
-                    We re not reinventing the wheel
-                  </h3>
-                  <p className="my-6 dark:text-gray-400">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Dolor aliquam possimus quas, error esse quos.
-                  </p>
-                  <button type="button" className="self-start">
-                    Action
+                  <button
+                    type="button"
+                    className="font-semibold self-start text-blue-800"
+                  >
+                    See More....
                   </button>
                 </div>
               </div>
